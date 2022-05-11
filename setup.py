dc4d1c512e511ae58cedb7205c9de44bccd48a1c
Sylvan LE DEUNFF <sledeunf@gmail.com>
2022-05-11 02:07:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name="pyclean",
-  description="CLI tool used to clean project before merge"
+  description="CLI tool used to clean project before merge",
   version="0.0.1",
   packages=setuptools.find_packages(),
   install_requires=['autoflake', 'black', 'click', 'isort', 'pylint'],
